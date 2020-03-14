bd3f76d3f41b3f59ccfc5d412b986fb7b94a98e9
zhouguiming <tr111999@126.com>
2020-03-14 23:48:18 +0800

    +

--- flag/flag.go
+++ flag/flag.go
@@ -17,15 +17,18 @@ var (
 	VerOpt     = flag.Bool("v", false, "Print application version")
 )
 
-// init 初始化
-// go build -ldflags "-X main.GitVersion=36fe168 -X main.GitBranch=develop"
+// go build -ldflags "-X github.com/keemis/library/flag.GitVersion=36fe168"
 func init() {
 	flag.Parse()
 	if *VerOpt {
+		stamp := "unknown"
 		t, _ := strconv.ParseInt(BuildStamp, 10, 64)
+		if t > 0 {
+			stamp = time.Unix(t, 0).Format("2006-01-02 15:04:05")
+		}
 		fmt.Printf("\tGitVersion: %v \n", GitVersion)
 		fmt.Printf("\tGitBranch: %v \n", GitBranch)
-		fmt.Printf("\tBuildStamp: %v \n", time.Unix(t, 0).Format("2006-01-02 15:04:05"))
+		fmt.Printf("\tBuildStamp: %v \n", stamp)
 		fmt.Printf("\tGoVersion: %v \n", GoVersion)
 		fmt.Printf("\tSysUname: %v \n", SysUName)
 		os.Exit(0)
