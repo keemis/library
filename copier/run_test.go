47256112e883cb1914b0aa398edd54c1d3793dfc
zhouguiming <tr111999@126.com>
2020-03-17 17:41:36 +0800

    +

---
+++ copier/run_test.go
@@ -0,0 +1,35 @@
+package copier
+
+import (
+	"testing"
+	"time"
+
+	"github.com/jinzhu/copier"
+)
+
+type User struct {
+	ID         int
+	Name       string
+	Age        int
+	CreateTime time.Time
+}
+
+type Employee struct {
+	ID     int
+	Name   string
+	Age    int
+	Role   string
+	Gender int
+}
+
+func TestRun(t *testing.T) {
+	user := User{
+		ID:   11,
+		Name: "simon",
+		Age:  23,
+	}
+	employee := Employee{}
+	_ = copier.Copy(&employee, &user)
+	t.Logf("user: %+v", user)
+	t.Logf("employee: %+v", employee)
+}
