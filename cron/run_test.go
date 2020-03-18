ca51b83d68a083a3b06ff6a8306610fcfdebdb35
zhouguiming <tr111999@126.com>
2020-03-18 14:50:51 +0800

    +

--- cron/run_test.go
+++ cron/run_test.go
@@ -1,9 +1,11 @@
 package cron
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/keemis/library/logs"
 	"github.com/robfig/cron/v3"
 )
 
@@ -11,13 +13,46 @@ import (
 // https://github.com/robfig/cron
 //
 
+type SyncJob struct {
+	log logs.Logger
+}
+
+func (s SyncJob) Run() {
+	fmt.Println("doing some sync ...")
+	time.Sleep(5 * time.Second)
+}
+
+type Log struct {
+}
+
+func (l Log) Info(msg string, keysAndValues ...interface{}) {
+	fmt.Println(msg, keysAndValues)
+}
+
+func (l Log) Error(err error, msg string, keysAndValues ...interface{}) {
+	fmt.Println(err, msg, keysAndValues)
+}
+
 func TestRun(t *testing.T) {
-	c := cron.New(cron.WithSeconds())
+	var l Log
+	c := cron.New(
+		cron.WithSeconds(),
+		cron.WithChain(
+			cron.Recover(l),            // 异常拉起
+			cron.SkipIfStillRunning(l), // 任务还在跑，就放弃本次
+		),
+	)
+
+	syncJob := SyncJob{
+		log: logs.New(),
+	}
+	c.AddJob("* * * * * *", syncJob)
+
 	c.AddFunc("* * * * * *", func() {
-		t.Logf("Every Second ")
+		fmt.Println("Every Second")
 	})
 
 	c.Start()
 
-	time.Sleep(1 * time.Hour)
+	time.Sleep(time.Hour)
 }
