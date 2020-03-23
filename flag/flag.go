393379b73fd1e672182d9b686ee1bb7a5895e46b
zhouguiming <tr111999@126.com>
2020-03-23 11:21:52 +0800

    +

--- flag/flag.go
+++ flag/flag.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/keemis/library/timer"
 )
 
 var (
@@ -14,7 +16,8 @@ var (
 	BuildStamp = "unknown"
 	GoVersion  = "unknown"
 	SysUName   = "unknown"
-	VerOpt     = flag.Bool("v", false, "Print application version")
+
+	VerOpt = flag.Bool("v", false, "Print application version")
 )
 
 // go build -ldflags "-X github.com/keemis/library/flag.GitVersion=36fe168"
@@ -24,8 +27,9 @@ func init() {
 		stamp := "unknown"
 		t, _ := strconv.ParseInt(BuildStamp, 10, 64)
 		if t > 0 {
-			stamp = time.Unix(t, 0).Format("2006-01-02 15:04:05")
+			stamp = time.Unix(t, 0).Format(timer.GoTimeFormat)
 		}
+		fmt.Printf("Version Information: \n")
 		fmt.Printf("\tGitVersion: %v \n", GitVersion)
 		fmt.Printf("\tGitBranch: %v \n", GitBranch)
 		fmt.Printf("\tBuildStamp: %v \n", stamp)
